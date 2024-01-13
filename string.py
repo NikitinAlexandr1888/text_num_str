79fbce71f884aef6b755bd948c04081c36ab05c9
AlexandrN <nikitinalexandr1888@yandex.ru>
2024-01-13 18:36:39 +0300

    Add recording

--- string.py
+++ string.py
@@ -8,8 +8,14 @@ def count_string(file_name):
   finally:
     file.close()
 
-    file_dict = {}
-    for file_name in file_list:
-        file_dict[file_name] = count_string(file_name)
-    return sorted(file_dict)
 
+file_dict = {}
+for file_name in file_dict:
+  file_dict[file_name] = count_string(file_name)
+sorted_files = sorted(file_dict)
+
+for file_name in sorted_files:
+  with open(file_name) as f:
+    lines = f.readlines()
+  with open('3.txt', 'a') as f3:
+    f3.writelines(lines)
